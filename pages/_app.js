e1786a7fbaec1138ac91a2f33b7d8db2b90d965c
kiwook shim <kiwook.shim@lge.com>
2026-05-25 14:08:38 +0900

    feat: make app installable as android home screen shortcut

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,25 @@
+import { useEffect } from 'react'
+import Head from 'next/head'
 import '../styles/globals.css'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if (typeof window === 'undefined' || !('serviceWorker' in navigator)) return
+    navigator.serviceWorker.register('/sw.js').catch(() => {})
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <meta name="theme-color" content="#d4c4a8" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="WORD & LIFE" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" href="/icon-192.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
